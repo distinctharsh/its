c5a16a643544599e56b8865b153bf2c8dbbcc89c
distinctharsh <distinctharsh@gmail.com>
2025-07-11 11:39:23 +0530

    Add logging for protection toggle and create settings migration

--- app/Http/Controllers/ProtectionController.php
+++ app/Http/Controllers/ProtectionController.php
@@ -4,15 +4,24 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Setting;
+use App\Services\LoggingService;
 
 class ProtectionController extends Controller
 {
     public function toggle(Request $request)
     {
         $enabled = $request->input('enabled') ? '1' : '0';
-        Setting::updateOrCreate(
+        $setting = Setting::updateOrCreate(
             ['key' => 'protection_enabled'],
             ['value' => $enabled]
+        );
+        // Log activity
+        LoggingService::logActivity(
+            $request,
+            'update',
+            'settings',
+            $setting->id,
+            ['key' => 'protection_enabled', 'value' => $enabled]
         );
         return response()->json(['success' => true]);
     }
